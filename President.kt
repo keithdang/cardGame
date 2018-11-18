03c5e062cb5702481caf09e5e31ea8d65e969573
keith <kdang@quartermaster.house>
2018-11-18 00:06:22 -0500

    User input precautions

--- President.kt
+++ President.kt
@@ -24,26 +24,34 @@ public class President{
                 if(Players.get(i-1).getHand().size==0){
                     gameContinue=false
                     println("Player ${i} wins")
+                    break
                 }
             }
         }
     }
-    fun playerTurn(num:Int){
+    private fun playerTurn(num:Int){
         val hand=Players.get(num-1).getHand()
+        print("Player: ${num}\n")
+        util.printCardsInLine(hand)
         val foundIndex:Int
-        if(turnCount==4) foundIndex=0
-        else foundIndex=getIndex(num,hand)
+
+        if(turnCount==4){
+            activeCards.clear()
+        }
+        foundIndex=getIndex(num,hand)
+
         var goAgain=false
         if(foundIndex!=-1){
+            hand.get(foundIndex).printCardLn()
             if(activeCards.size>0 && activeCards.get(0).getPerceivedValue()==hand.get(foundIndex).getPerceivedValue()){
                 println("BURN")
                 goAgain=true
+                activeCards.clear()
+            }else{
+                activeCards.clear()
+                activeCards.add(hand.get(foundIndex))
             }
-            activeCards.clear()
-            activeCards.add(hand.get(foundIndex))
             hand.removeAt(foundIndex)
-            util.printCardsInLineLabel(activeCards)
-            util.printCardsInLine(hand)
             if(goAgain){
                 playerTurn(num)
             }
@@ -53,29 +61,30 @@ public class President{
             turnCount++
         }
     }
-    fun getIndex(playerNum:Int,hand:MutableList<Cards>):Int{
-        val searchVal:Int
+    private fun getIndex(playerNum:Int,hand:MutableList<Cards>):Int{
         val foundIndex:Int
         if(playerNum==1){
-            print("Select Card (s for skip):\n")
-            val input=readLine()!!
-            if(input.equals("s")){
-                foundIndex=-1
-            }else{
-                searchVal = input.toInt()
-                foundIndex=searchVal-1
-            }
+            foundIndex=inputCard(hand)
         }else{
             if(activeCards.size==0){
                 foundIndex=0
             }else{
-                searchVal = activeCards.get(0).getPerceivedValue()
+                val searchVal = activeCards.get(0).getPerceivedValue()
                 foundIndex=util.searchCard(hand,0,hand.size-1,searchVal)
             }
         }
         return foundIndex
     }
-    fun presConvert(card:String):Int{
+    private fun inputCard(hand:MutableList<Cards>):Int{
+        print("Select Card (s for skip):\n")
+        val input=readLine()!!
+        when{
+            input.equals("s")->return -1
+            input.toIntOrNull()!=null && input.toInt() in 1..hand.size->return input.toInt() -1
+            else->return inputCard(hand)
+        }
+    }
+    private fun presConvert(card:String):Int{
         val newVal:Int
         when(card){
             "Jack"->newVal=11
